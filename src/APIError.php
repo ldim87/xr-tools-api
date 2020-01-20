31fe41aafe033cf513cfd6b22e5969a725382224
Admin <support@1000.menu>
2020-01-20 10:20:15 +0100

    Add Exceptions files

---
+++ src/APIError.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Oleg Isaev (PandCar)
+ * @contacts vk.com/id50416641, t.me/pandcar, github.com/pandcar
+ */
+
+namespace XrTools;
+
+class APIError extends \Exception {
+	
+}
---
+++ src/APIResponse.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Oleg Isaev (PandCar)
+ * @contacts vk.com/id50416641, t.me/pandcar, github.com/pandcar
+ */
+
+namespace XrTools;
+
+class APIResponse extends \Exception {
+	
+	private $message_data;
+
+	/**
+	 * Json encodes the message and calls the parent constructor.
+	 *
+	 * @param null           $message
+	 * @param int            $code
+	 * @param Exception|null $previous
+	 */
+	public function __construct($message, $code = 0, \Exception $previous = null)
+	{
+		if(is_array($message)){
+			$this->message_data = $message;
+			$message = 'See message data via getData() method';
+		} else {
+			$this->message_data = null;
+		}
+
+		parent::__construct($message, $code, $previous);
+	}
+
+	/**
+	 * Returns the json decoded message.
+	 *
+	 * @param bool $assoc
+	 *
+	 * @return mixed
+	 */
+	public function getData()
+	{
+		return $this->message_data ?? $this->getMessage();
+	}
+}
